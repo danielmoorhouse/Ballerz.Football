7f5249c5a86a4259cd6d68560279c4973b474f4b
danielmoorhouse <danmoorhouse80@gmail.com>
2019-03-22 15:03:44 +0000

    Add models, viewmodels, identity

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+using System;
+using Ballerz.Football.Ballerz.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Ballerz.Football.Areas.Identity.IdentityHostingStartup))]
+namespace Ballerz.Football.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- Ballerz.Web/Controllers/ContinentController.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Mvc;
-
-namespace Ballerz.Football.Ballerz.Web.Controllers
-{
-    public class ContinentController : Controller
-    {
-        
-    }
-}
